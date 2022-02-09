7a6a57335cb56c82324741169c6681d9596d824e
Diana da Silva Nunes <dianadsn@gmail.com>
2022-02-09 20:54:41 -0300

    Exercicio 1000

---
+++ 1000/Main.java
@@ -0,0 +1,13 @@
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ */
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+       System.out.println("Hello World!");
+ 
+    }
+ 
+}
